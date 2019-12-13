b3c606a1ff510f4f7ed865cf9659807bc35bce04
phetos90 <56889350+phetos90@users.noreply.github.com>
2019-12-13 10:54:48 +0900

    Update botkorean.py

--- botkorean.py
+++ botkorean.py
@@ -2,11 +2,10 @@ import discord
 
 client = discord.Client()
 
-
 @client.event
 async def on_message(message):
-    if message.content.startswith("안녕"):
-        await message.channel.send("할말")
+    if message.content.startswith("hi"):
+        await message.channel.send("hihi")
 
 
 
