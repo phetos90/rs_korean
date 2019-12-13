18e8206844b09a70d454e3a0540dbda871040038
phetos90 <56889350+phetos90@users.noreply.github.com>
2019-12-13 16:51:53 +0900

    Update botkorean.py

--- botkorean.py
+++ botkorean.py
@@ -1,13 +1,21 @@
 import discord
+import os
+
 
 client = discord.Client()
 
 
 @client.event
-async def on_message(message):
-    if message.content.startswith("안녕"):
-        await message.channel.send("할말")
+async def on_ready():
+    print("login")
+    print(client.user.name)
+    print(client.user.id)
+    await client.change_persence(game=discord.Game(name='', type=1))
 
+@client.event
+async def on_message(message):
+    if message.content.startswith("hi"):
+        await message.channel.send("HI")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
